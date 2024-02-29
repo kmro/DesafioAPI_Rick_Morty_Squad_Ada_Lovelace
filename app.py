567156e8be4a922321fafdfd6a82d10d8b923c9b
LorraneAlkimim <lorranealkmim@yahoo.com.br>
2024-02-29 02:41:07 -0300

    feat: adiciona links dos perfis nas paginas de localizacao e episodio, ajusta importacoes e estilizacao do projeto

--- app.py
+++ app.py
@@ -3,29 +3,39 @@ import urllib.request, json
 
 app = Flask(__name__)
 
-#obter lista personagens
-@app.route('/')
+
+# Obter lista personagens
+@app.route("/")
 def get_list_characters_pages():
     url = "https://rickandmortyapi.com/api/character/"
     response = urllib.request.urlopen(url)
     data = response.read()
-    dict = json.loads(data) 
+    dict = json.loads(data)
 
     return render_template("characters.html", characters=dict["results"])
 
-# obter perfil único
-@app.route('/profile/<id>')
+
+# Obter perfil único
+@app.route("/profile/<id>")
 def get_profile(id):
     url = "https://rickandmortyapi.com/api/character/" + id
     response = urllib.request.urlopen(url)
     data = response.read()
-    dict = json.loads(data) 
+    dict = json.loads(data)
 
     return render_template("profile.html", profile=dict)
 
 
-#obter lista de personagens
-@app.route('/lista')
+def get_character_name(id):
+    url = "https://rickandmortyapi.com/api/character/" + id
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    return dict["name"]
+
+
+# Obter lista de personagens
+@app.route("/lista")
 def get_list_characters():
 
     url = "https://rickandmortyapi.com/api/character/"
@@ -37,51 +47,48 @@ def get_list_characters():
     for character in dict["results"]:
         character = {
             "name": character["name"],
-            "status":character["status"],
-            "especie":character["spercies"],
-            "genero":character["gender"],
-            "origem":character["location"],
-
-
+            "status": character["status"],
+            "especie": character["spercies"],
+            "genero": character["gender"],
+            "origem": character["location"],
         }
 
         characters.append(character)
 
     return {"characters": characters}
 
-#Rota para listar as localizações
-@app.route('/locations')
+
+# Obter lista de localizações
+@app.route("/locations")
 def get_list_locations():
     url = "https://rickandmortyapi.com/api/location"
     response = urllib.request.urlopen(url)
     locations = response.read()
     dict = json.loads(locations)
     locations_list = []
- #LISTA DE LOCALIZAÇÃO
-    
+
     for location in dict["results"]:
         location_dict = {
             "Nome": location["name"],
             "Tipo": location["type"],
-            "Dimensão":location["dimension"],
-            "Personagens":location["residents"],
-            "codigo":location["id"]
+            "Dimensão": location["dimension"],
+            "Personagens": location["residents"],
+            "codigo": location["id"],
         }
         locations_list.append(location_dict)
-        #print(location_dict)
-       
-    return render_template("locations.html",locations=locations_list)
 
-#Rota para listar as Episódios
-@app.route('/episodes')
+    return render_template("locations.html", locations=locations_list)
+
+
+# Obter lista de episódios
+@app.route("/episodes")
 def get_list_episodes():
     url = "https://rickandmortyapi.com/api/episode"
     response = urllib.request.urlopen(url)
     episodes = response.read()
     dict = json.loads(episodes)
     episodes_list = []
- #LISTA DE EPISÓDIOS
-    
+
     for episode in dict["results"]:
         episode_dict = {
             "Nome": episode["name"],
@@ -89,28 +96,31 @@ def get_list_episodes():
             "Código": episode["id"],
         }
         episodes_list.append(episode_dict)
-        #print(episode_dict)
-       
-    return render_template("episodes.html",episodes=episodes_list)
 
-# obter Localização única
+    return render_template("episodes.html", episodes=episodes_list)
+
+
+# Obter Localização única
 @app.route("/location/<id>")
 def get_single_location(id):
     url = "https://rickandmortyapi.com/api/location/" + id
     response = urllib.request.urlopen(url)
     data = response.read()
     single_location = json.loads(data)
-    
-    #print(data)
 
-    return render_template("location.html",  location = single_location)
-#obter episódio unico
+    return render_template(
+        "location.html", location=single_location, name=get_character_name
+    )
+
+
+# Obter episódio único
 @app.route("/episode/<id>")
 def get_single_episode(id):
     url = "https://rickandmortyapi.com/api/episode/" + id
-    #url = f"https://rickandmortyapi.com/api/episode/{id}"
     response = urllib.request.urlopen(url)
     data = response.read()
     single_episode = json.loads(data)
 
-    return render_template("episode.html", episode = single_episode)
+    return render_template(
+        "episode.html", episode=single_episode, name=get_character_name
+    )
